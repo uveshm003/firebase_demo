fc05b7433556b8af14e59b1921455f57a6393b43
uveshm003 <uveshmenpur.03@gmail.com>
2025-05-09 22:58:47 +0530

    setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
+import 'package:firebase_demo/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Container());
-  }
 }
